a42bc3d74a474b6e49176cd37956d1a8247ced49
Diogo Silas Mendes Ribeiro <108630083+DiogoSilas@users.noreply.github.com>
2023-02-10 17:36:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const openMenu = window.document.querySelector('#openMenu')
+const closeMenu = window.document.querySelector('#closeMenu')
+const menu = window.document.getElementById('menu')
+
+window.alert('COM ROLAMENTO VERTICAL LIVRE!. Todo o layout pode ter o rolamento vertical livre, ou seja, se adapta ao conteudo. Para isso remova todas as propriedade HEIGTH  de todos os elementos container')
+
+
+
+openMenu.addEventListener('click', () => {
+    menu.style.display = 'flex'
+    menu.style.right = (menu.offsetWidth * -1) + ' px'
+    openMenu.style.display = 'none'
+
+    setTimeout(() => {
+        menu.style.opacity = '1'
+
+        menu.style.right = '0'
+    }, 10)
+})
+
+closeMenu.addEventListener('click', () => {
+    menu.style.opacity = '0'
+
+    menu.style.right = (menu.offsetWidth * -1) + 'px'
+    setTimeout(() => {
+        menu.removeAttribute('style')
+
+        openMenu.removeAttribute('style')
+    }, 200)
+})
